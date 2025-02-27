87c2016c5ed44baf6e443997d71b6f080fb38170
msolpera <msolpera@gmail.com>
2025-02-27 11:33:48 -0300

    Eliminar valores mayores al corte en median house value

--- housing_model.py
+++ housing_model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f9831cc57c6e3566dcd82713b4300c6a58182aaace0ed3f5f4028f57cb50b3f
-size 32449
+oid sha256:31e445d6364581e0bdbe25da9ed18b49ab9b119ec3a1d457a0d136e25a8c7496
+size 32565
